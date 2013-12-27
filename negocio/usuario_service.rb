e591c0e2d17f89e0985eb02721fc8bff13f839f2
javi <mediomelocoton@gmail.com>
2013-12-27 17:22:01 +0100

    test_vista_usuario

---
+++ negocio/usuario_service.rb
@@ -0,0 +1,5 @@
+require_relative '../datos/UsuarioDAO'
+
+class UsuarioService
+
+end
--- test/web/test_servidor_plantilla.rb
+++ test/web/test_servidor_plantilla.rb
@@ -27,40 +27,31 @@ end
   def test_aparece_el_logo_de_la_pagina_de_inicio
     get '/index'
     assert last_response.ok?
-    assert last_response.body.include?('muevete')
+    body_includes 'muevete'
   end
 
   def test_se_muestra_las_peticiones_con_su_imagen_titulo_firmasConseguidas_enlace
     get '/index'
     assert last_response.ok?
-    assert last_response.body.include?('1.jpg')
-    assert last_response.body.include?('Que se proteja al berberecho salvaje')
-    assert last_response.body.include?('Firmas conseguidas')
-    assert last_response.body.include?('peticion?id=1')
+    body_includes '1.jpg', 'Que se proteja al berberecho salvaje', 'Firmas conseguidas', 'peticion?id=1'
   end
 
   def test_aparecen_botones_de_login_y_registro
     get '/index'
     assert last_response.ok?
-    assert last_response.body.include?('Login')
-    assert last_response.body.include?('Register')
+    body_includes 'Login', 'Register'
   end
 
   def test_comprobar_campos_del_login
     get '/index'
     assert last_response.ok?
-    assert last_response.body.include?('Usuario')
-    assert last_response.body.include?('Contraseña')
-    assert last_response.body.include?('Entrar')
+    body_includes 'Usuario', 'Contraseña', 'Entrar'
   end
 
   def test_comprobar_campos_del_registro
     get '/index'
     assert last_response.ok?
-    assert last_response.body.include?('Email')
-    assert last_response.body.include?('Password')
-    assert last_response.body.include?('Confirmar Password')
-    assert last_response.body.include?('Registrar')
+    body_includes 'Email', 'Password', 'Confirmar Password', 'Registrar'
   end
 
   def test_acceder_a_la_vista_de_peticiones
@@ -68,4 +59,38 @@ end
     assert last_response.ok?
   end
 
+  def test_aparecen_el_titulo_de_la_peticion
+    get '/peticion', :id => '1'
+    body_includes 'Que se proteja al berberecho salvaje'
+  end
+
+  def test_aparece_la_imagen_que_representa_la_peticion
+    get '/peticion', :id => '1'
+    body_includes '1.jpg'
+  end
+
+  def test_nombre_apellidos_foto_del_creador_de_la_peticion
+    get '/peticion', :id => '1'
+    body_includes 'adi@ua.es.jpg', 'Alfonso', 'David Iradier', 'usuario?login=adi@ua.es'
+  end
+
+  def test_texto_de_la_peticion_en_parrafos
+    get 'peticion', :id => '1'
+    body_includes 'Desde tiempos inmemoriales los humanos han cazado a los berberechos sin piedad'
+  end
+
+  def test_comprobar_campos_para_la_firma_de_peticiones
+    get 'peticion', :id => '1'
+    body_includes 'Nombre', 'Apellidos', 'Email', 'Motivo', 'Mostar públicamente mi firma ', 'firmar'
+  end
+
+  def test_ver_firmas_conseguidas_y_necesarias
+    get 'peticion', :id => '1'
+    body_includes 'Firmas conseguidas', 'Firmas necesarias'
+  end
+
+  def test_acceder_vista_usuario
+    get 'usuario', :login =>  'adi@ua.es'
+    assert last_response.ok?
+  end
 end
